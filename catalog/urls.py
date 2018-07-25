f79e9d9958ed5920c93f599ddf702ff5c4b879fe
gentaliana <gentaliana@gmail.com>
2018-07-25 19:12:43 +0300

    index

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from . import views
+from django.urls import path
 
 
 urlpatterns = [
-
+    path('', views.index, name='index'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import EventsDescript, EventsGift, Users
 
-# Create your views here.
+
+def index(request):
+    num_events_descript = EventsDescript.objects.all().count()
+    num_users = Users.objects.all().count()
+    num_events_gift = EventsGift.objects.all().count()
+
+    # Отрисовка HTML-шаблона index.html с данными внутри
+    # переменной контекста context
+
+    return render(
+        request,
+        'index.html',
+        context={'num_events_descript': num_events_descript, 'num_users': num_users, 'num_events_gift': num_events_gift},
+    )
